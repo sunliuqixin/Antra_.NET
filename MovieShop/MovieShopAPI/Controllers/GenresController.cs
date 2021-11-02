83bbc458cb8c0ae50fee4aaf90cb0a6c29ae7c8b
sunliuqixin <sunliuqixin@gmail.com>
2021-11-02 07:45:09 -0700

    Added API--GenresController

---
+++ MovieShop/MovieShopAPI/Controllers/GenresController.cs
@@ -0,0 +1,43 @@
+﻿using ApplicationCore.ServiceInterfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly IGenreService _genreService;
+
+        public GenresController(IGenreService genreService)
+        {
+            _genreService = genreService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        // Attribute based routing
+        // http://localhost/api/movies/toprevenue
+        // API
+        public async Task<IActionResult> GetTopRevenueMovies()
+        {
+            var genres = await _genreService.GetAll();
+
+            // JSON data and Http Status Code //
+
+            if (!genres.Any())
+            {
+                // return 404
+                return NotFound("No Genres Found");
+            }
+
+            // 200 OK
+            return Ok(genres);
+        }
+        }
+}
